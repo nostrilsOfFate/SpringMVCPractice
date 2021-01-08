147c99c6e7910402dd97c934f217d47aaae08f54
aleksandrova <aleksandrova2@lanit.ru>
2021-01-08 15:59:52 +0300

    Controllers

--- src/main/java/practice/HelloController.java
+++ src/main/java/practice/HelloController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
-    @GetMapping("/hello-world")
-    public String sayHello(){
-        return "hello World";
-    }
+//
+//    @GetMapping("/hello-world")
+//    public String sayHello(){
+//        return "hello_world";
+//    }
 }
---
+++ src/main/java/practice/config/MySpringMvcDicspatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package practice.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDicspatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
